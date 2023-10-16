2239bc1c83deb000f983d748f3672bbd7c9175dc
Pedro Nevarez <pedro_nevarez_tz@hotmail.com>
2023-10-15 20:02:14 -0600

    fixed issues

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+//import { FaGithub } from "react-icons/fa";
 import Input from "@/components/Input";
 import { toast } from "react-toastify";
 
@@ -127,13 +127,13 @@ const Auth = () => {
               >
                 <FcGoogle size={30} />
               </div>
-              <div
+              {/* <div
                 onClick={() => signIn("github", { callbackUrl: "/" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center 
                justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={30} />
-              </div>
+              </div> */}
             </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
